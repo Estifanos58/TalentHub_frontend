847a303ebca7567f1a4b15cddd4a78b84328f926
Estifanos58 <estifkebe08@gmail.com>
2025-08-24 14:00:23 +0300

    fix(deployemnt): oprional data fetching added

--- src/app/employer/jobs/page.tsx
+++ src/app/employer/jobs/page.tsx
@@ -7,7 +7,7 @@ const EmployerJobsPage = async() => {
   // console.log('EmployerJobsPage user response:', response);
   return (
     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
-      <EmployerJobContent userId={response.data._id}/>
+      <EmployerJobContent userId={response.data?._id}/>
     </Suspense>
   )
 }
